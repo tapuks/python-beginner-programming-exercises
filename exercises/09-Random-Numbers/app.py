7ccafba7196e7915ce5378ff17d6dca2f7f46b61
tapuks <david_berdiell@hotmail.com>
2021-06-01 19:31:00 +0000

    exercise 10

--- exercises/09-Random-Numbers/app.py
+++ exercises/09-Random-Numbers/app.py
@@ -2,7 +2,8 @@ import random
 
 def get_randomInt():
   # CHANGE ONLY THIS LINE BELOW
-	random_number = random.random()
+	# random_number = random.random(1,10)
+    random_number = random.randint(1,10)
 	return random_number
 
 
